be8657b18dcb716d8f02284d3a078d4ca70a02c8
Chris Barts <chbarts@gmail.com>
2017-02-10 11:06:24 -0700

    Rearrange beheader.c

--- beheader.c
+++ beheader.c
@@ -4,15 +4,14 @@
 
 #include "beheader.h"
 
-G_DEFINE_TYPE_WITH_CODE (GBeheader, g_beheader, G_TYPE_OBJECT,
-			 G_IMPLEMENT_INTERFACE (G_TYPE_CONVERTER,
-						g_beheader_iface_init))
-
-
 static bool seen_hend = false;
 static int ncrs = 0, nnls = 0;
 
 static void g_beheader_iface_init          (GConverterIface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (GBeheader, g_beheader, G_TYPE_OBJECT,
+			 G_IMPLEMENT_INTERFACE (G_TYPE_CONVERTER,
+						g_beheader_iface_init))
 
 static void
 g_beheader_class_init (GBeheaderClass *klass)
